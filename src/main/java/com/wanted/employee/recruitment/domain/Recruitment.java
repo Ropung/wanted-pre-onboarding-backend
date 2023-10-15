e4033ea581c68583a11f3f5c38c6ccfd39ebc8c7
Ropung <85475762+Ropung@users.noreply.github.com>
2023-10-16 05:29:50 +0900

    feat(entity): Add - anotation - update method

--- src/main/java/com/wanted/employee/recruitment/domain/Recruitment.java
+++ src/main/java/com/wanted/employee/recruitment/domain/Recruitment.java
@@ -1,11 +1,21 @@
 package com.wanted.employee.recruitment.domain;
 
 import com.wanted.employee.support.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 
+import javax.persistence.Entity;
 import java.time.Instant;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
 public class Recruitment extends BaseEntity {
     private Long companyId;
     private String companyName;
@@ -16,7 +26,27 @@ public class Recruitment extends BaseEntity {
     private String technology;
     private String contents;
     @CreatedDate
+    @lombok.Builder.Default
     private Instant createdAt = Instant.now();
     @UpdateTimestamp
     private Instant updatedAt;
+
+    public void changePosition(String position){
+        this.position = position;
+    }
+
+    public void changeCompensation(Long compensation){
+        this.compensation = compensation;
+    }
+    public void changeTechnology(String technology){
+        this.technology = technology;
+    }
+    public void changeContents(String contents){
+        this.contents = contents;
+    }
+
+
+
+
+
 }
